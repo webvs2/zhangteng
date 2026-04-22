75f2b8b58ec47e6d9599c59a498caf47331849b4
yang <2636098325@qq.com>
2026-04-22 21:32:23 +0800

    222

--- cloudflare-workflow/src/index.ts
+++ cloudflare-workflow/src/index.ts
@@ -1,10 +1,19 @@
 export { MyWorkflow } from "./workflow";
 
+const WORKFLOW_PATH = /^\/api\/workflow\/?$/;
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
-    const instanceId = url.searchParams.get("instanceId");
 
+    if (!WORKFLOW_PATH.test(url.pathname)) {
+      if (url.pathname.startsWith("/api/")) {
+        return new Response("Not Found", { status: 404 });
+      }
+      return env.ASSETS.fetch(request);
+    }
+
+    const instanceId = url.searchParams.get("instanceId");
     if (instanceId) {
       const instance = await env.MY_WORKFLOW.get(instanceId);
       return Response.json(await instance.status());
@@ -22,7 +31,11 @@ export default {
       return Response.json({ instanceId: instance.id });
     }
 
-    const instance = await env.MY_WORKFLOW.create();
-    return Response.json({ instanceId: instance.id });
+    if (request.method === "GET") {
+      const instance = await env.MY_WORKFLOW.create();
+      return Response.json({ instanceId: instance.id });
+    }
+
+    return new Response("Method Not Allowed", { status: 405 });
   },
 } satisfies ExportedHandler<Env>;
